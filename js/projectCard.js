85f5063a83a1951b55fdbbf0ff2155927a9edab0
zthoffman21 <zachhoffman@ymail.com>
2024-09-24 16:03:54 -0400

    deleted old code

--- js/projectCard.js
+++ js/projectCard.js
@@ -1,31 +1,3 @@
-// document.querySelectorAll('.projectCardContainer').forEach((card) => {
-//   card.addEventListener('mousemove', (e) => {  
-//     e.preventDefault();
-    
-//     const rect = card.getBoundingClientRect();
-//     const cardWidth = card.offsetWidth;
-//     const cardHeight = card.offsetHeight;
-//     const centerX = rect.left + cardWidth / 2;
-//     const centerY = rect.top + cardHeight / 2;
-    
-//     // Calculate the mouse position relative to the card's center
-//     const offsetX = e.clientX - centerX;
-//     const offsetY = e.clientY - centerY;
-
-//     // Normalize the mouse position relative to the card dimensions
-//     const rotateX = (offsetY / (cardHeight / 2)) * -10; // Vertical rotation
-//     const rotateY = (offsetX / (cardWidth / 2)) * 10;   // Horizontal rotation
-
-//     // Apply the rotation and scale
-//     card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.1)`;
-//   });
-
-//   card.addEventListener('mouseleave', () => {
-//     // Reset the transformation when the mouse leaves
-//     card.style.transform = 'rotateX(0) rotateY(0) scale(1)';
-//   });
-// });
-
 document.querySelectorAll('.projectCardContainer').forEach((card) => {
   const lightEffect = document.createElement('div');
   lightEffect.classList.add('lightEffect');
