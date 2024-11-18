dd11d8620b9b52c2fc906632d30699173d9dae2d
zthoffman21 <zachhoffman@ymail.com>
2024-11-18 14:26:07 -0500

    added themes and local storage for themes. I still need to add a popup when the theme button is clicked. For now, it just cycles through themes

---
+++ js/theme.js
@@ -0,0 +1,19 @@
+const themeButton = document.getElementById("themeButton");
+const themes = ["default", "light", "pumpkin", "blue"];
+var currentTheme = 0;
+
+// Set theme on them button press
+if (themeButton) {
+    themeButton.addEventListener('click', function() {
+        currentTheme = (currentTheme + 1) % themes.length;
+        document.documentElement.setAttribute("dataTheme", themes[currentTheme]);
+        localStorage.setItem("theme", currentTheme);
+    });
+}
+
+// Load theme on page load
+document.addEventListener("DOMContentLoaded", function() {
+    if (localStorage.getItem("theme")) {
+        this.documentElement.setAttribute("dataTheme", themes[parseInt(localStorage.getItem("theme"), 10)]);
+    }
+});
